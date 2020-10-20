9ebabe17ba5cefe84ae33b659124e2214a661be2
Mateusz Ostapiuk <ostek@vp.pl>
2020-10-20 14:30:31 +0200

    Dodanie do pliku Main.java "Hello world", utworzenie klasy PeopleStats oraz Person (Podpunkt 3)

---
+++ src/com/company/Person.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class Person {
+    private final String firstName;
+
+    public Person(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
