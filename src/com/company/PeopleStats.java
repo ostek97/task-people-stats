ec5584fec4c5ba83d2417c5211376ebfc38304c1
Mateusz Ostapiuk <ostek@vp.pl>
2020-10-20 14:35:23 +0200

    Dodanie możliwości wczytywania osób z pliku w klasie PeopleStats (Podpunkt 5)

--- src/com/company/PeopleStats.java
+++ src/com/company/PeopleStats.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class PeopleStats {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class PeopleStats {
+    private final List<Person> people;
+
+    public PeopleStats(Path inputFilePath) {
+        try {
+            people = Files.lines(inputFilePath)
+                    .map(line -> line.split("\t"))
+                    .map(chunks -> new Person(chunks[0], chunks[1]))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
